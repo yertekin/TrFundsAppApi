607c8af45189d8a7e52877a39cde40daddc0b63e
yertekin <ertekin.yigit@gmail.com>
2023-01-07 23:14:11 +0300

    Initial commit

---
+++ src/main/java/tr/fundsapp/TrFundsAppApi/TrFundsAppApiApplication.java
@@ -0,0 +1,13 @@
+package tr.fundsapp.TrFundsAppApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrFundsAppApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrFundsAppApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tr/fundsapp/TrFundsAppApi/TrFundsAppApiApplicationTests.java
@@ -0,0 +1,13 @@
+package tr.fundsapp.TrFundsAppApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrFundsAppApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
